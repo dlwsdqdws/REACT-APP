d29d4a196eb33100ef976e2ec053a66539d0fe4e
“Lei <dlwsdqdws@gmail.com>
2022-01-01 02:31:29 -0800

    react-transition-group

--- src/App.js
+++ src/App.js
@@ -1,27 +1,47 @@
 import React, {Component, Fragment} from 'react';
+import { CSSTransition , TransitionGroup} from 'react-transition-group';
 import './App.css';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      show : true
+      list : []
     }
-    this.handleToggle = this.handleToggle.bind(this);
+    this.handleAddItem = this.handleAddItem.bind(this);
   }
 
   render() {
     return (
-      <Fragment>
-        <div className = {this.state.show? 'show' : 'hide'}>Hello</div>
-        <button onClick = {this.handleToggle}>toggle</button>
+      <Fragment>       
+        <TransitionGroup>
+        {
+          this.state.list.map((item, index) => {
+            return (
+              <CSSTransition
+                timeout = {1000}
+                classNames = 'fade'
+                unmountOnExit
+                onEntered = {(el) => {el.style.color = 'blue'}}
+                appear = {true}
+                key = {index}
+              > 
+                <div>{item}</div>
+              </CSSTransition> 
+            )
+          })
+        }
+        </TransitionGroup>
+        <button onClick = {this.handleAddItem}>toggle</button>
       </Fragment>
     )
   }
 
-  handleToggle() {
-    this.setState({
-      show : this.state.show? false : true
+  handleAddItem() {
+    this.setState((prevState) => {
+      return {
+        list : [...prevState.list, 'item']
+      }
     })
   }
 }
