cebb106286dce47f3eaf780914f5074005e6c920
“Lei <dlwsdqdws@gmail.com>
2022-01-03 00:27:58 -0800

    store

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,38 +1,61 @@
 import React, {Component, Fragment} from 'react';
 import 'antd/dist/antd.css';
-import { Input } from 'antd';
-import { Button } from 'antd';
-import { List, Typography, Divider } from 'antd';
+import { Input, Button, List,} from 'antd';
 import axois from 'axios';
+import store from './store';
 
-const data = [
-    'Racing car sprays burning fuel into crowd.',
-    'Japanese princess to wed commoner.',
-    'Australian walks 100km after outback crash.',
-    'Man charged over missing wedding girl.',
-    'Los Angeles battles huge wildfires.',
-  ];
 
 class TodoList extends Component {
+    constructor(props){
+        super(props);
+        this.state = store.getState();
+        console.log(this.state);
+        this.handleInputChange = this.handleInputChange.bind(this);
+        store.subscribe(() => {
+            this.setState(store.getState())
+        });
+        this.handleBtnClick = this.handleBtnClick.bind(this);
+    }
+
     render() {
         return (
             <div style = {{marginTop : '10px', marginLeft : '10px'}}>
                 <div>
-                    <Input placeholder = 'todo info' style = {{width : '300px', marginRight : '10px'}} />
-                    <Button type="primary">Submit</Button>
+                    <Input 
+                        value = {this.state.inputValue} 
+                        placeholder = 'todo info' 
+                        style = {{width : '300px', marginRight : '10px'}} 
+                        onChange = {this.handleInputChange}
+                    />
+                    <Button type="primary" onClick = {this.handleBtnClick} >Submit</Button>
                 </div>
                 <List
-                style = {{marginTop : '10px', width : '300px'}}
-                bordered
-                dataSource={data}
-                renderItem={item => (
-                    <List.Item>
-                    <Typography.Text mark>[ITEM]</Typography.Text> {item}
-                    </List.Item>
-                )}
+                    style = {{marginTop : '10px', width : '300px'}}
+                    bordered
+                    dataSource={this.state.list}
+                    renderItem={item => (
+                        <List.Item>
+                        {item}
+                        </List.Item>
+                    )}
                 />
             </div>
         )
+    }
+
+    handleInputChange(e) {
+        const action = {
+            type : 'change_input_value',
+            value : e.target.value
+        }
+        store.dispatch(action);
+    }
+
+    handleBtnClick() {
+        const action = {
+            type : 'add_todo_item'
+        }
+        store.dispatch(action);
     }
 }
 
