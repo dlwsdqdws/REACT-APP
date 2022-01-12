81563de9d8c830d4571146352be21dd91c5e6e14
“Lei <dlwsdqdws@gmail.com>
2022-01-12 14:21:40 -0800

    header-style

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -1,7 +1,12 @@
 import React, {Component} from 'react';
 import {
     HeaderWrapper,
-    Logo
+    Logo,
+    Nav,
+    NavItem,
+    NavSearch,
+    Addition,
+    Button
 } from "./style";
 
 class Header extends Component {
@@ -9,6 +14,17 @@ class Header extends Component {
         return (
             <HeaderWrapper>
                 <Logo />
+                <Nav>
+                    <NavItem className = 'left active'>Home</NavItem>
+                    <NavItem className = 'left'>Download App</NavItem>
+                    <NavItem className = 'right'>Login</NavItem>
+                    <NavItem className = 'right'>Aa</NavItem>
+                    <NavSearch></NavSearch>
+                </Nav>
+                <Addition>
+                    <Button className = 'writing'>Post</Button>
+                    <Button className = 'reg'>Signup</Button>
+                </Addition>
             </HeaderWrapper>
         )
     }
