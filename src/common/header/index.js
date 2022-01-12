f9981d81632998442de7338376026173bc54d3c6
“Lei <dlwsdqdws@gmail.com>
2022-01-12 13:28:39 -0800

    logo

---
+++ src/common/header/index.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import {
+    HeaderWrapper,
+    Logo
+} from "./style";
+
+class Header extends Component {
+    render() {
+        return (
+            <HeaderWrapper>
+                <Logo />
+            </HeaderWrapper>
+        )
+    }
+}
+
+export default Header;
