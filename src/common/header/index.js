af3e8ba90950e4872b0febdbb718b3f146f5fdb6
“Lei <dlwsdqdws@gmail.com>
2022-01-13 01:44:31 -0800

    hotsearch-axios

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import {connect} from 'react-redux';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { CSSTransition } from 'react-transition-group';
-import  {actionCreators} from './store';
+import { actionCreators } from './store';
 import {
     HeaderWrapper,
     Logo,
@@ -10,63 +10,95 @@ import {
     NavSearch,
     Addition,
     Button,
-    SearchWrapper
+    SearchWrapper,
+    SearchInfo,
+    SearchInfoTitle,
+    SearchInfoSwitch,
+    SearchInfoItem,
+    SearchInfoList
 } from "./style";
 
-const Header = (props) => {
-    return (
-        <HeaderWrapper>
-            <Logo />
-            <Nav>
-                <NavItem className = 'left active'>Home</NavItem>
-                <NavItem className = 'left'>Download App</NavItem>
-                <NavItem className = 'right'>Login</NavItem>
-                <NavItem className = 'right'>
-                    <span className = "iconfont">&#xe636;</span>
-                </NavItem>
-                <SearchWrapper>
-                    <CSSTransition
-                        in = {props.focused}
-                        timeout = {200}
-                        classNames = "slide"
-                    >
-                        <NavSearch
-                            className = {props.focused ? 'focused' : ''}
-                            onFocus = {props.handleInputFocus}
-                            onBlur = {props.handleInputBlur}
-                        ></NavSearch>
-                    </CSSTransition>
-                    <span className = {props.focused ? 'focused iconfont' : 'iconfont'}>&#xe617;</span>
-                </SearchWrapper>
-            </Nav>
-            <Addition>
-                <Button className = 'writing'>
-                    <span className = "iconfont">&#xe600;</span>
-                    Post
-                </Button>
-                <Button className = 'reg'>Sign up</Button>
-            </Addition>
-        </HeaderWrapper>
-    );
+
+class Header extends Component {
+
+    getListArea ()  {
+        if (this.props.focused) {
+            return (
+                <SearchInfo>
+                    <SearchInfoTitle>
+                        Hot
+                        <SearchInfoSwitch>Change</SearchInfoSwitch>
+                    </SearchInfoTitle>
+                    <SearchInfoList>
+                        {
+                            this.props.list.map((item) => {
+                                return <SearchInfoItem key = {item}>{item}</SearchInfoItem>
+                            })
+                        }
+                    </SearchInfoList>
+                </SearchInfo>
+            )
+        } else {
+            return null;
+        }
+    }
+
+    render() {
+        return (
+            <HeaderWrapper>
+                <Logo />
+                <Nav>
+                    <NavItem className='left active'>Home</NavItem>
+                    <NavItem className='left'>Download App</NavItem>
+                    <NavItem className='right'>Login</NavItem>
+                    <NavItem className='right'>
+                        <span className="iconfont">&#xe636;</span>
+                    </NavItem>
+                    <SearchWrapper>
+                        <CSSTransition
+                            in={this.props.focused}
+                            timeout={200}
+                            classNames="slide"
+                        >
+                            <NavSearch
+                                className={this.props.focused ? 'focused' : ''}
+                                onFocus={this.props.handleInputFocus}
+                                onBlur={this.props.handleInputBlur}
+                            ></NavSearch>
+                        </CSSTransition>
+                        <span className={this.props.focused ? 'focused iconfont' : 'iconfont'}>&#xe617;</span>
+                        {this.getListArea()}
+                    </SearchWrapper>
+                </Nav>
+                <Addition>
+                    <Button className='writing'>
+                        <span className="iconfont">&#xe600;</span>
+                        Post
+                    </Button>
+                    <Button className='reg'>Sign up</Button>
+                </Addition>
+            </HeaderWrapper>
+        );
+    }
 }
 
 
 const mapStateToProps = (state) => {
     return {
-        focused : state.getIn(['header', 'focused'])
+        focused: state.getIn(['header', 'focused']),
+        list : state.getIn(['header', 'list'])
     }
 }
 
 const mapDispatchToProps = (dispatch) => {
     return {
         handleInputFocus() {
-            const action = actionCreators.searchFocus();
-            dispatch(action);
+            dispatch(actionCreators.getList())
+            dispatch(actionCreators.searchFocus());
         },
 
         handleInputBlur() {
-            const action = actionCreators.searchBlur();
-            dispatch(action);
+            dispatch(actionCreators.searchBlur());
         }
     }
 }
