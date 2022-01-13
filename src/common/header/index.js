de34232098157776ffbddfdcdd888eb285faa071
“Lei <dlwsdqdws@gmail.com>
2022-01-13 01:54:44 -0800

    opt

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -22,7 +22,8 @@ import {
 class Header extends Component {
 
     getListArea ()  {
-        if (this.props.focused) {
+        const {focused, list} = this.props;
+        if (focused) {
             return (
                 <SearchInfo>
                     <SearchInfoTitle>
@@ -31,7 +32,7 @@ class Header extends Component {
                     </SearchInfoTitle>
                     <SearchInfoList>
                         {
-                            this.props.list.map((item) => {
+                            list.map((item) => {
                                 return <SearchInfoItem key = {item}>{item}</SearchInfoItem>
                             })
                         }
@@ -44,6 +45,7 @@ class Header extends Component {
     }
 
     render() {
+        const {focused, handleInputFocus, handleInputBlur} = this.props;
         return (
             <HeaderWrapper>
                 <Logo />
@@ -56,17 +58,17 @@ class Header extends Component {
                     </NavItem>
                     <SearchWrapper>
                         <CSSTransition
-                            in={this.props.focused}
+                            in={focused}
                             timeout={200}
                             classNames="slide"
                         >
                             <NavSearch
-                                className={this.props.focused ? 'focused' : ''}
-                                onFocus={this.props.handleInputFocus}
-                                onBlur={this.props.handleInputBlur}
+                                className={focused ? 'focused' : ''}
+                                onFocus={handleInputFocus}
+                                onBlur={handleInputBlur}
                             ></NavSearch>
                         </CSSTransition>
-                        <span className={this.props.focused ? 'focused iconfont' : 'iconfont'}>&#xe617;</span>
+                        <span className={focused ? 'focused iconfont' : 'iconfont'}>&#xe617;</span>
                         {this.getListArea()}
                     </SearchWrapper>
                 </Nav>
