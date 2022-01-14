935a77669d74b6e81c982fe3f5542feb3a25772c
“Lei <dlwsdqdws@gmail.com>
2022-01-13 20:17:48 -0800

    router-dom

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import { Provider } from 'react-redux';
+import { BrowserRouter, Routes } from 'react-router-dom';
 import Header from './common/header';
-import {GolbalIcon} from './statics/iconfont/iconfont';
+import { GolbalIcon } from './statics/iconfont/iconfont';
 import store from './store';
 
-class App extends Component{
+class App extends Component {
     render() {
         return (
-            <Provider store = {store}>
-                <Header />
-                <GolbalIcon />
+            <Provider store={store}>
+                <div>
+                    <Header />
+                    <GolbalIcon />
+
+                    <BrowserRouter>
+                        <div>
+                            <Routes path='/' exact component={Home}></Routes>
+                            <Routes path='/detail' component={Detail}></Routes>
+                        </div>
+                    </BrowserRouter>
+                </div>
             </Provider>
         );
     }
